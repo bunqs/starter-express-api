f56e5c2cf17800532fb86aa07d4effbf645cfd8c
bunqs <136875512+bunqs@users.noreply.github.com>
2023-06-27 20:12:08 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
 
-
+        console.log(req.headers["accept-language"])
         res.redirect('https://blog.grupocajamar.com/las-formas-de-entender-el-dinero-en-las-distintas-culturas/?gf=54806369')
 
 })
